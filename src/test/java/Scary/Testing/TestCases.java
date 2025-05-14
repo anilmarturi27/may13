20f20c2bc9b26f09fcb50eb369bd617ee8a5e4cd
anilmarturi27 <anilmarturi0@gmail.com>
2025-05-14 10:44:56 +0530

    Suggestions case updated

--- src/test/java/Scary/Testing/TestCases.java
+++ src/test/java/Scary/Testing/TestCases.java
@@ -57,118 +57,118 @@ public class TestCases extends Box {
 	}
 	
 	
-//	@Test(priority=2,dependsOnMethods="Createdoc")
-//	public void CreateDocFromFile() throws InterruptedException {
-//		
-//		Signin sn = new Signin(driver);
-//		DocumentHomePage dhm = new DocumentHomePage(driver);
-//		WindowHandle wh = new WindowHandle(driver);
-//		
-////		Creating the document from document File menu
-//		boolean FileMenuDoc=dhm.DocFromFileMenu();
-//		Assert.assertTrue( FileMenuDoc,"document is not created from file menu");
-//		
-////		Checking the created file menu document
-//		boolean Checked = dhm.DocumentCheck();
-//		Assert.assertTrue(Checked);
-//		driver.close();
-//		
-////		Switching the document to the main document
-//		boolean switched = wh.switchTo(DocumentName);
-//		Assert.assertTrue(switched);
-//	}
-////	
-//	@Test(priority=3,dependsOnMethods="Createdoc")
-//	public void MakeACopy() throws InterruptedException {
-//		DocumentHomePage dhm = new DocumentHomePage(driver);
-//		WindowHandle wh = new WindowHandle(driver);
-//		dhm.MakeACopyFromFileMenu();
-//		System.out.println(driver.getTitle());
-//		Thread.sleep(5000);
-//		boolean switched = wh.switchTo("copy");
-////		Set<String> s = driver.getWindowHandles();
-////		List<String> list = new ArrayList<>();
-////		Iterator<String> itr = s.iterator();
-////		while(itr.hasNext()) {
-////			String ss = itr.next();
-////			driver.switchTo().window(ss);
-////			list.add(driver.getTitle());
-////		}
-////		
-////		System.out.println(list);
-//		Assert.assertTrue(switched);
-//		boolean Checked = dhm.DocumentCheck();
-//		Assert.assertTrue(Checked);
-//		driver.close();
-//		
-////		Switching the document to the main document
-//		boolean switchedd = wh.switchTo(DocumentName);
-//		Assert.assertTrue(switchedd);
-//	}
-////		
-//	@Test(priority=4,dependsOnMethods="Createdoc")
-//	public void TextEntering() throws InterruptedException {
-//		DocumentHomePage dhm = new DocumentHomePage(driver);
-//		dhm.EnterText("Hello Anil Marturi");
-//	}
+	@Test(priority=2,dependsOnMethods="Createdoc")
+	public void CreateDocFromFile() throws InterruptedException {
+		
+		Signin sn = new Signin(driver);
+		DocumentHomePage dhm = new DocumentHomePage(driver);
+		WindowHandle wh = new WindowHandle(driver);
+		
+//		Creating the document from document File menu
+		boolean FileMenuDoc=dhm.DocFromFileMenu();
+		Assert.assertTrue( FileMenuDoc,"document is not created from file menu");
+		
+//		Checking the created file menu document
+		boolean Checked = dhm.DocumentCheck();
+		Assert.assertTrue(Checked);
+		driver.close();
+		
+//		Switching the document to the main document
+		boolean switched = wh.switchTo(DocumentName);
+		Assert.assertTrue(switched);
+	}
 //	
-//	@Test(priority=5,dependsOnMethods="Createdoc")
-//	public void InsertDrawing() throws InterruptedException {
-//		DocumentHomePage dhm = new DocumentHomePage(driver);
-//		dhm.DrwaingInsert();
-//		boolean Checked = dhm.DocumentCheck();
-//		Assert.assertTrue(Checked);
-//		//switching the driver to the default content because documentcheck method returns boolean and i am unable to switch to default content in that method itself
-//		driver.switchTo().defaultContent();
-//	}
-////	
-//	@Test(priority=6)
-//	public void comment() throws InterruptedException {
-//		DocumentHomePage dhm = new DocumentHomePage(driver);
-//		dhm.InsertComment("This is automated comment1");
-//		dhm.InsertComment("comment2");
-//		dhm.ClickOnCommentStream();
-//		dhm.CommentReply("This is automated comment","Reply 1");
-//		dhm.ClickOnCommentStream();
-//		dhm.ResolveComment("This is automated comment");
-//		dhm.ClickOnCommentStream();
-//		dhm.ReOpenComment("This is automated comment");
-//		boolean Checked = dhm.DocumentCheck();
-//		Assert.assertTrue(Checked);
+	@Test(priority=3,dependsOnMethods="Createdoc")
+	public void MakeACopy() throws InterruptedException {
+		DocumentHomePage dhm = new DocumentHomePage(driver);
+		WindowHandle wh = new WindowHandle(driver);
+		dhm.MakeACopyFromFileMenu();
+		System.out.println(driver.getTitle());
+		Thread.sleep(5000);
+		boolean switched = wh.switchTo("copy");
+//		Set<String> s = driver.getWindowHandles();
+//		List<String> list = new ArrayList<>();
+//		Iterator<String> itr = s.iterator();
+//		while(itr.hasNext()) {
+//			String ss = itr.next();
+//			driver.switchTo().window(ss);
+//			list.add(driver.getTitle());
+//		}
+//		
+//		System.out.println(list);
+		Assert.assertTrue(switched);
+		boolean Checked = dhm.DocumentCheck();
+		Assert.assertTrue(Checked);
+		driver.close();
+		
+//		Switching the document to the main document
+		boolean switchedd = wh.switchTo(DocumentName);
+		Assert.assertTrue(switchedd);
+	}
+//		
+	@Test(priority=4,dependsOnMethods="Createdoc")
+	public void TextEntering() throws InterruptedException {
+		DocumentHomePage dhm = new DocumentHomePage(driver);
+		dhm.EnterText("Hello Anil Marturi");
+	}
+	
+	@Test(priority=5,dependsOnMethods="Createdoc")
+	public void InsertDrawing() throws InterruptedException {
+		DocumentHomePage dhm = new DocumentHomePage(driver);
+		dhm.DrwaingInsert();
+		boolean Checked = dhm.DocumentCheck();
+		Assert.assertTrue(Checked);
+		//switching the driver to the default content because documentcheck method returns boolean and i am unable to switch to default content in that method itself
+		driver.switchTo().defaultContent();
+	}
+//	
+	@Test(priority=6)
+	public void comment() throws InterruptedException {
+		DocumentHomePage dhm = new DocumentHomePage(driver);
+		dhm.InsertComment("This is automated comment1");
+		dhm.InsertComment("comment2");
+		dhm.ClickOnCommentStream();
+		dhm.CommentReply("This is automated comment","Reply 1");
+		dhm.ClickOnCommentStream();
+		dhm.ResolveComment("This is automated comment");
+		dhm.ClickOnCommentStream();
+		dhm.ReOpenComment("This is automated comment");
+		boolean Checked = dhm.DocumentCheck();
+		Assert.assertTrue(Checked);
+		Actions action = new Actions(driver);
+		action.keyDown(Keys.CONTROL).sendKeys(Keys.END).keyUp(Keys.CONTROL).build().perform();
+		driver.switchTo().defaultContent();
+		
+		//switching the driver to the default content because documentcheck method returns boolean and i am unable to switch to default content in that method itself
+		driver.switchTo().defaultContent();
+		
+	}
+	
+	
+	@Test(priority=7)
+	public void InsertHorizontalLine() throws InterruptedException {
+		DocumentHomePage dhm = new DocumentHomePage(driver);
+		dhm.HorizontalLine();
+		boolean Checked = dhm.DocumentCheck();
+		Assert.assertTrue(Checked);
+		//switching the driver to the default content because documentcheck method returns boolean and i am unable to switch to default content in that method itself
+		driver.switchTo().defaultContent();
+		}
+	
+	@Test(priority=8)
+	public void InsertSpecialCharacter() throws InterruptedException {
+		DocumentHomePage dhm = new DocumentHomePage(driver);
+		dhm.SpecialCharacters();
+		boolean Checked = dhm.DocumentCheck();
+		Assert.assertTrue(Checked);
+		//switching the driver to the default content because documentcheck method returns boolean and i am unable to switch to default content in that method itself
+		
 //		Actions action = new Actions(driver);
 //		action.keyDown(Keys.CONTROL).sendKeys(Keys.END).keyUp(Keys.CONTROL).build().perform();
-//		driver.switchTo().defaultContent();
-//		
-//		//switching the driver to the default content because documentcheck method returns boolean and i am unable to switch to default content in that method itself
-//		driver.switchTo().defaultContent();
-//		
-//	}
-//	
-//	
-//	@Test(priority=7)
-//	public void InsertHorizontalLine() throws InterruptedException {
-//		DocumentHomePage dhm = new DocumentHomePage(driver);
-//		dhm.HorizontalLine();
-//		boolean Checked = dhm.DocumentCheck();
-//		Assert.assertTrue(Checked);
-//		//switching the driver to the default content because documentcheck method returns boolean and i am unable to switch to default content in that method itself
-//		driver.switchTo().defaultContent();
-//		}
-//	
-//	@Test(priority=8)
-//	public void InsertSpecialCharacter() throws InterruptedException {
-//		DocumentHomePage dhm = new DocumentHomePage(driver);
-//		dhm.SpecialCharacters();
-//		boolean Checked = dhm.DocumentCheck();
-//		Assert.assertTrue(Checked);
-//		//switching the driver to the default content because documentcheck method returns boolean and i am unable to switch to default content in that method itself
-//		
-////		Actions action = new Actions(driver);
-////		action.keyDown(Keys.CONTROL).sendKeys(Keys.END).keyUp(Keys.CONTROL).build().perform();
-////		driver.switchTo().defaultContent();
-//	}
+		driver.switchTo().defaultContent();
+	}
 	
-	@Test(priority=2)
+	@Test(priority=9)
 	public void SuggestionMode() {
 		DocumentHomePage dhm = new DocumentHomePage(driver);
 		
